7f5562e131edc8d0357c1c2a4fdcfc22cd70e9a2
Eliel Godoy <eliel.godoy@etec.sp.gov.br>
2023-03-03 16:52:19 -0300

    Laravel com Bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
 <body>
 
     <div class="container py-4 px-3 mx-auto">
-        <h1>Hello, Bootstrap and Vite!</h1>
+        <h1>Utilizando Bootstrap e Vite no Laravel!</h1>
         <button class="btn btn-primary">
             Primary button
         </button>
@@ -29,22 +29,56 @@
         <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#SegundoPasso" aria-expanded="false" aria-controls="collapseExample">
             Segundo passo
         </button>
+        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#TerceiroPasso" aria-expanded="false" aria-controls="collapseExample">
+            Terceiro passo
+        </button>
+        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#TerceiroPasso" aria-expanded="false" aria-controls="collapseExample">
+            Terceiro passo
+        </button>
     </div>
 
-    <div class="" style="width: 500px; padding-left: 130px;">
+    <div class="" style="width: 600px; padding-left: 130px;">
+
         <div class="collapse" id="PrimeiroPasso">
             <div class="card card-body">
                 Primeiro crie o projeto Laravel com o composer usando:<br>
                 composer create-project laravel/laravel NomeDoProjeto
             </div>
         </div>
+
         <br>
+
         <div class="collapse" id="SegundoPasso">
             <div class="card card-body">
-                Primeiro crie o projeto Laravel com o composer usando:<br>
-                composer create-project laravel/laravel NomeDoProjeto
+                Navegue até a pasta do projeto usando:<br>
+                cd NomeDoProjeto<br><br>
+                Instale os pacotes necessários executando:<br>
+                npm install<br><br>
             </div>
         </div>
+
+        <br>
+
+        <div class="collapse" id="TerceiroPasso">
+            <div class="card card-body">
+                Instale o bootstrap inserindo:<br>
+                npm i bootstrap @popperjs/core<br><br>
+                Depois o sass:<br>
+                npm i --save-dev sass<br><br>
+            </div>
+        </div>
+
+        <br>
+
+        <div class="collapse" id="QuartoPasso">
+            <div class="card card-body">
+                Instale o bootstrap inserindo:<br>
+                npm i bootstrap @popperjs/core<br><br>
+                Depois o sass:<br>
+                npm i --save-dev sass<br><br>
+            </div>
+        </div>
+
     </div>
     
 </body>
